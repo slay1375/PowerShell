4a2a67b70c4652eef1398963191aefd55b1ad556
PlayStation1 <45473988+PlayStation1@users.noreply.github.com>
2018-12-04 10:48:55 -0500

    Update Detecting PSSessions.ps1

--- Detecting PSSessions.ps1
+++ Detecting PSSessions.ps1
@@ -1,4 +1,16 @@
-﻿Get-EventLog security | where-object {$_.eventid -eq "4624"} | select -expandproperty message | ForEach-Object {
+<#
+    .SYNOPSIS
+        Finds established PowerShell sessions. 
+
+        THIS CODE IS MADE AVAILABLE AS IS, WITHOUT WARRANTY OF ANY KIND. THE ENTIRE 
+    	RISK OF THE USE OR THE RESULTS FROM THE USE OF THIS CODE REMAINS WITH THE USER.
+
+    .DESCRIPTION
+        Finds Type 3 logons with a logon process name "NtLmSsp" which is associated with remote powershell sessions.
+
+#>
+
+Get-EventLog security | where-object {$_.eventid -eq "4624"} | select -expandproperty message | ForEach-Object {
 
 if ($_ -like "*Logon Type:  3*" -and $_ -like "*Logon Process:  NtLmSsp*"){
     Write-Output $_
