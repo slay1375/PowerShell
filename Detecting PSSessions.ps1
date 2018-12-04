713d890cc434fa13dd8de37aa3f6668cc08f4777
PlayStation1 <45473988+PlayStation1@users.noreply.github.com>
2018-12-04 10:08:01 -0500

    Add files via upload

---
+++ Detecting PSSessions.ps1
@@ -0,0 +1,6 @@
+﻿Get-EventLog security | where-object {$_.eventid -eq "4624"} | select -expandproperty message | ForEach-Object {
+
+if ($_ -like "*Logon Type:  3*" -and $_ -like "*Logon Process:  NtLmSsp*"){
+    Write-Output $_
+    }
+}
