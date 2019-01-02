9f9cadf7b03c05be07c2a9a8d8a4ca3e01225e54
PlayStation1 <45473988+PlayStation1@users.noreply.github.com>
2019-01-02 15:10:09 -0500

    Update Get-ADUser.ps1

--- Active Directory/Get-ADUser.ps1
+++ Active Directory/Get-ADUser.ps1
@@ -1,3 +1,15 @@
+<#
+
+Description:
+
+  - Query Active Directory user accounts by a specific creation date.
+  
+Example:
+
+  - An example is provided below querying users on the "PlayStation1.example" domain from 01JAN2018 - 31JAN2018.
+
+#>
+
 $Start = Get-Date -Day 01 -Month 01 -Year 2018 -Hour 00 
 
 $End = Get-Date -Day 31 -Month 01 -Year 2018 -Hour 23 -Minute 59 
