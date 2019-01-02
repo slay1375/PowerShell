aba41b9ac3fbf88d34e8cf5e8dfc3b3506b60b5d
PlayStation1 <45473988+PlayStation1@users.noreply.github.com>
2019-01-02 15:12:44 -0500

    Update Get-ADUser.ps1

--- Active Directory/Get-ADUser.ps1
+++ Active Directory/Get-ADUser.ps1
@@ -14,4 +14,4 @@ $Start = Get-Date -Day 01 -Month 01 -Year 2018 -Hour 00
 
 $End = Get-Date -Day 31 -Month 01 -Year 2018 -Hour 23 -Minute 59 
 
-Get-ADUser -server "PlayStation1.example" -Filter * -Properties whenCreated | Where-Object { ($_.whenCreated -gt $Start) -and ($_.whenCreated -le $End) } | export-csv "test.csv"
+Get-ADUser -server "PlayStation1.example" -Filter * -Properties whenCreated | Where-Object { ($_.whenCreated -gt $Start) -and ($_.whenCreated -le $End) } | export-csv "Users.csv"
