58ea331df09103a56cc1519d6d908617ddf408f4
PlayStation1 <45473988+PlayStation1@users.noreply.github.com>
2019-01-08 10:31:56 -0500

    Update and rename Detect-PSSessions to Detect-PSSessions.ps1

--- Event Log/Detect-PSSessions.ps1
+++ Event Log/Detect-PSSessions.ps1
@@ -4,6 +4,14 @@
         
 .Description:
     - Finds Type 3 logons with a logon process name "NtLmSsp" which is associated with remote powershell sessions.
+
+.Notes  
+    File Name      : Detect-PSSessions.ps1
+    Version        : v.1.0
+    Author         : @PlayStation1
+    Prerequisite   : Windows PowerShell 5.0
+    Created        : January 08, 2019
+
 #>
 
 Get-EventLog security | where-object {$_.eventid -eq "4624"} | select -expandproperty message | ForEach-Object {
