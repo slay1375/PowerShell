397bef3ac8c8c074d4c9bb78e58b72be3a89f4e3
PlayStation1 <45473988+PlayStation1@users.noreply.github.com>
2019-02-07 15:16:13 -0500

    Update Get-Wrecked.ps1

--- Processes/Get-Wrecked.ps1
+++ Processes/Get-Wrecked.ps1
@@ -8,34 +8,37 @@
 #>
 
 clear
-$Process = Get-WmiObject -Class Win32_Process
+$ProcessList = Get-WmiObject -Class Win32_Process
 
-foreach ($Proc in $Process){
+foreach ($Process in $ProcessList){
             
-    $ParentProcessName =  (get-wmiobject -Class Win32_process | Where-Object {$_.processid -eq $Proc.parentprocessid}).name
+    $ParentProcessName =  (get-wmiobject -Class Win32_process | Where-Object {$_.processid -eq $Process.parentprocessid}).name
+    $SID = ($Process).getownersid().SID
+    $User = ($Process).getowner().user
+    $Domain = ($Process).getowner().Domain
         
-        if ($Proc.processname -eq "System Idle Process" -and($Proc.ProcessId -ne "0" -or $Proc.ParentProcessId -ne "0")){
+        if ($Process.processname -eq "System Idle Process" -and($Process.ProcessId -ne "0" -or $Process.ParentProcessId -ne "0")){
                  #System Idle Process
                  Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
                  Write-Host " System Idle process" -ForegroundColor DarkYellow "`n"
                  Write-Host "     - System Idle Process should have a ProcessID of 0 and a ParentProcessID of 0.`n"
         }
         
-        if ($Proc.processname -eq "System" -and ($Proc.ProcessId -ne "4" -or $Proc.ParentProcessId -ne "0")){
+        if ($Process.processname -eq "System" -and ($Process.ProcessId -ne "4" -or $Process.ParentProcessId -ne "0")){
                  #System
                  Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
                  Write-Host " System" -ForegroundColor DarkYellow "`n"
                  Write-Host "     - System should have a ProcessID of 4 and a ParentProcessID of 0.`n"
         }
         
-        if ($Proc.processname -eq "smss.exe" -and ($Proc.ParentProcessId -ne "4")){
+        if ($Process.processname -eq "smss.exe" -and ($Process.ParentProcessId -ne "4")){
                  #smss.exe
                  Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
                  Write-Host " smss.exe" -ForegroundColor DarkYellow "`n"
                  Write-Host "     - smss.exe should have a ParentProcessID of 4.`n"
         }         
         
-        if ($Proc.processname -eq "wininit.exe" -and ($ParentProcessName)){
+        if ($Process.processname -eq "wininit.exe" -and ($ParentProcessName)){
                  
                  #wininit.exe
                  Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
@@ -43,7 +46,7 @@ foreach ($Proc in $Process){
                  Write-Host "     - wininit.exe should have no ParentProcessID.`n"
         }  
        
-        if ($Proc.processname -eq "winlogon.exe" -and ($ParentProcessName)){
+        if ($Process.processname -eq "winlogon.exe" -and ($ParentProcessName)){
                  
                  #winlogon.exe
                  Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
@@ -51,7 +54,7 @@ foreach ($Proc in $Process){
                  Write-Host "     - winlogon.exe should have no ParentProcessID.`n"
         } 
 
-        if ($Proc.processname -eq "csrss.exe" -and ($ParentProcessName)){
+        if ($Process.processname -eq "csrss.exe" -and ($ParentProcessName)){
                  
                  #csrss.exe
                  Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
@@ -59,16 +62,15 @@ foreach ($Proc in $Process){
                  Write-Host "     - csrss.exe should have no ParentProcessID.`n"
         } 
          
-    ($Proc | select ProcessName, ProcessID, @{n="ParentProcessName";e={ $ParentProcessName }}, ParentProcessID, Path, CommandLine, @{n="StartTime";e={ $_.ConvertToDateTime($Proc.creationdate) }}  | Format-List | Out-String).trim()
-    
-    foreach ($establishedIP in (Get-NetTCPConnection | Where-Object {$_.owningprocess -eq $Proc.processid -and $_.state -eq "Established"})){
+    ($Process | select ProcessName, ProcessID, @{n="ParentProcessName";e={ $ParentProcessName }}, ParentProcessID, Path, CommandLine, @{n="StartTime";e={ $_.ConvertToDateTime($Process.creationdate) }}, @{n="User";e={ $User }}, @{n="SID";e={ $SID }}, @{n="Domain";e={ $Domain }}  | Format-List | Out-String).trim()
+
+    foreach ($establishedIP in (Get-NetTCPConnection | Where-Object {$_.owningprocess -eq $Process.processid -and $_.state -eq "Established"})){
         Write-Host "Established       : $($establishedIP.LocalAddress):$($establishedIP.LocalPort)  ===>  $($establishedIP.RemoteAddress):$($establishedIP.RemotePort)"
     } 
     
-    foreach ($establishedIP in (Get-NetTCPConnection | Where-Object {$_.owningprocess -eq $Proc.processid -and $_.state -eq "Listen"})){
+    foreach ($establishedIP in (Get-NetTCPConnection | Where-Object {$_.owningprocess -eq $Process.processid -and $_.state -eq "Listen"})){
         Write-Host "Listening         : $($establishedIP.LocalAddress):$($establishedIP.LocalPort)  ===>  $($establishedIP.RemoteAddress):$($establishedIP.RemotePort)"
     } 
 
     Write-Host "___________________________________________________________________________________________________________________________________`n"
-
 }
