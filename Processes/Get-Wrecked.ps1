e2ac68f7d41534e111151ee9068caf89668eb596
PlayStation1 <45473988+PlayStation1@users.noreply.github.com>
2019-02-08 14:57:10 -0500

    Update Get-Wrecked.ps1

--- Processes/Get-Wrecked.ps1
+++ Processes/Get-Wrecked.ps1
@@ -5,15 +5,24 @@
     Author         : @PlayStation1; @TerrySmithMBA
     Prerequisite   : Windows PowerShell 5.0
     Created        : January 14, 2019
-#>
-
-# System Idle Process: Process ID, Parent Process ID
-# System: Process ID, Parent Process ID
-# smss.exe: Parent ID, User, Path
-# wininit.exe: Parent Process Name, Count, User, Path
-# winlogon.exe: Parent Process Name, User, Path
-# csrss.exe: Parent Process Name, User, Path
-# svchost.exe: Parent Process Name, Path
+<#
+System Idle Process: 
+    - Process ID, Parent Process ID
+System: 
+    - Process ID, Parent Process ID
+smss.exe:
+    - Parent ID, User, Path
+wininit.exe:
+    - Parent Process Name, Count, User, Path
+winlogon.exe:
+    - Parent Process Name, User, Path
+csrss.exe:
+    - Parent Process Name, User, Path
+svchost.exe:
+    - Parent Process Name, Path
+services.exe:
+    - Parent Process Name, Count, User, Path
+#> 
 
 clear
 
@@ -27,6 +36,7 @@ foreach ($Process in $ProcessList){
     $Domain = ($Process).getowner().Domain
     $Path = $Process | select path
     $Wininit = Get-Process | Where-Object {$_.Name -eq "wininit"}
+    $services = Get-Process | Where-Object {$_.Name -eq "services"}
        
         if ($Process.ProcessName -eq "System Idle Process" -and ($Process.ProcessId -ne "0" -or $Process.ParentProcessId -ne "0")){
 
@@ -35,7 +45,8 @@ foreach ($Process in $ProcessList){
                  Write-Host " System Idle process" -ForegroundColor DarkYellow "`n"
                  Write-Host "     - System Idle Process should have a ProcessID of 0 and a ParentProcessID of 0.`n"
         }
-
+#___________________________________________________________________________________________________________________________________________
+        
         if ($Process.ProcessName -eq "System" -and ($Process.ProcessId -ne "4" -or $Process.ParentProcessId -ne "0")){
 
                  #System: Process ID = 4 Parent Process = 0
@@ -43,6 +54,7 @@ foreach ($Process in $ProcessList){
                  Write-Host " System" -ForegroundColor DarkYellow "`n"
                  Write-Host "     - System should have a ProcessID of 4 and a ParentProcessID of 0.`n"
         }
+#___________________________________________________________________________________________________________________________________________
 
         if ($Process.ProcessName -eq "smss.exe" -and ($Process.ParentProcessId -ne "4")){
 
@@ -67,13 +79,14 @@ foreach ($Process in $ProcessList){
                  Write-Host " smss.exe" -ForegroundColor DarkYellow "`n"
                  Write-Host "     - smss.exe should should be located in C:\windows\system32\smss.exe.`n"
         }
+#___________________________________________________________________________________________________________________________________________
 
         if ($Process.ProcessName -eq "wininit.exe" -and ($ParentProcessName)){
 
                  #wininit.exe: Parent Process = Created by an instance of smss.exe that exits, so tools usually do not provide the parent process name.
                  Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
                  Write-Host " wininit.exe" -ForegroundColor DarkYellow "`n"
-                 Write-Host "     - wininit.exe should have no ParentProcessID.`n"
+                 Write-Host "     - wininit.exe should have no Parent Process.`n"
         } 
 
         if ($Process.ProcessName -eq "wininit.exe" -and ($Wininit.count -gt 1)){
@@ -99,13 +112,14 @@ foreach ($Process in $ProcessList){
                  Write-Host " smss.exe" -ForegroundColor DarkYellow "`n"
                  Write-Host "     - wininit.exe should should be located in C:\windows\system32\wininit.exe.`n"
         }
+#___________________________________________________________________________________________________________________________________________
 
         if ($Process.ProcessName -eq "winlogon.exe" -and ($ParentProcessName)){
 
                  #winlogon.exe: Parent Process = Created by an instance of smss.exe that exits, so tools usually do not provide the parent process name.
                  Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
                  Write-Host " winlogon.exe" -ForegroundColor DarkYellow "`n"
-                 Write-Host "     - winlogon.exe should have no ParentProcessID.`n"
+                 Write-Host "     - winlogon.exe should have no Parent Process.`n"
         } 
 
         if ($Process.ProcessName -eq "winlogon.exe" -and ($User -ne "SYSTEM")){
@@ -123,13 +137,14 @@ foreach ($Process in $ProcessList){
                  Write-Host " winlogon.exe" -ForegroundColor DarkYellow "`n"
                  Write-Host "     - winlogon.exe should should be located in C:\windows\system32\winlogon.exe.`n"
         }
+#___________________________________________________________________________________________________________________________________________
 
         if ($Process.ProcessName -eq "csrss.exe" -and ($ParentProcessName)){
 
                  #csrss.exe: Parent Process = Created by an instance of smss.exe that exits, so tools usually do not provide the parent process name.
                  Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
                  Write-Host " csrss.exe" -ForegroundColor DarkYellow "`n"
-                 Write-Host "     - csrss.exe should have no ParentProcessID.`n"
+                 Write-Host "     - csrss.exe should have no Parent Process.`n"
         }
 
         if ($Process.ProcessName -eq "csrss.exe" -and ($User -ne "SYSTEM")){
@@ -147,21 +162,55 @@ foreach ($Process in $ProcessList){
                  Write-Host " csrss.exe" -ForegroundColor DarkYellow "`n"
                  Write-Host "     - csrss.exe should should be located in C:\windows\system32\csrss.exe.`n"
         }
+#___________________________________________________________________________________________________________________________________________
 
         if ($Process.ProcessName -eq "svchost.exe" -and ($ParentProcessName -ne "services.exe")){
+
+                 #svchost.exe: Parent Process = services.exe
+                 Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
+                 Write-Host " svchost.exe" -ForegroundColor DarkYellow "`n"
+                 Write-Host "     - svchost.exe should have a Parent Process named services.exe.`n"
+        }
+
+        if ($Process.ProcessName -eq "svchost.exe" -and ($Path.ToString()) -and ($Path -ne "C:\windows\system32\svchost.exe")){    
 
                  #svchost.exe: Path = C:\windows\system32\svchost.exe
                  Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
                  Write-Host " svchost.exe" -ForegroundColor DarkYellow "`n"
                  Write-Host "     - svchost.exe should should be located in C:\windows\system32\svchost.exe.`n"
         }
+#___________________________________________________________________________________________________________________________________________
 
-        if ($Process.ProcessName -eq "svchost.exe" -and ($Path.ToString()) -and ($Path -ne "C:\windows\system32\svchost.exe")){    
+        if ($Process.ProcessName -eq "services.exe" -and ($ParentProcessName -ne "wininit.exe")){
 
-                 #csrss.exe: Path = C:\windows\system32\svchost.exe
+                 #services.exe: Parent Process = wininit.exe
                  Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
-                 Write-Host " svchost.exe" -ForegroundColor DarkYellow "`n"
-                 Write-Host "     - svchost.exe should should be located in C:\windows\system32\svchost.exe.`n"
+                 Write-Host " services.exe" -ForegroundColor DarkYellow "`n"
+                 Write-Host "     - services.exe should have a Parent Process named wininit.exe.'n"
+        }
+
+        if ($Process.ProcessName -eq "services.exe" -and ($services.count -gt 1)){
+
+                 #services.exe: Count = 1
+                 Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
+                 Write-Host " services.exe" -ForegroundColor DarkYellow "`n"
+                 Write-Host "     - There should only be one instance of services.exe running.`n"
+        }
+
+        if ($Process.ProcessName -eq "services.exe" -and ($User -ne "SYSTEM")){
+
+                 #services.exe: User Account = SYSTEM or NT AUTHORITY
+                 Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
+                 Write-Host " services.exe" -ForegroundColor DarkYellow "`n"
+                 Write-Host "     - services.exe should have a User of SYSTEM or NT AUTHORITY.`n"
+        } 
+
+        if ($Process.ProcessName -eq "services.exe" -and ($Path.ToString()) -and ($Path -ne "C:\windows\system32\services.exe")){    
+
+                 #services.exe: Path = C:\windows\system32\services.exe
+                 Write-Host "Potential threat found with process: " -ForegroundColor DarkYellow -NoNewline
+                 Write-Host " services.exe" -ForegroundColor DarkYellow "`n"
+                 Write-Host "     - services.exe should should be located in C:\windows\system32\services.exe.`n"
         }
 
     ($Process | select ProcessName, ProcessID, @{n="ParentProcessName";e={ $ParentProcessName }}, ParentProcessID, Path, CommandLine, @{n="StartTime";e={ $_.ConvertToDateTime($Process.creationdate) }}, @{n="User";e={ $User }}, @{n="SID";e={ $SID }}, @{n="Domain";e={$Domain }}  | Format-List | Out-String).trim()
